11425ab0146109c73114678f1a0fcc49c83f4c34
Jesus Chacon <chjesussao@gmail.com>
2020-03-10 22:45:14 -0400

    agregado html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,150 +5,162 @@ const postcssPresetEnv = require('postcss-preset-env');
 const path = require('path');
 
 module.exports = {
-    devtool: 'source-map',
-    entry: {
-        app: ['@babel/polyfill','./src/main.js'],
-    },
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname,'dist'),
-        filename: 'js/[name].js'
-    },
-    devServer: {
-        contentBase: path.join(__dirname,'dist'),
-        compress: true,
-        open: true
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            title: 'My App',
-            template: './src/index.html',
-            filename: 'index.html',
-            minify: {
-                collapseWhitespace: false,
-                removeComments: true,
-                removeRedundantAttributes: true,
-                removeScriptTypeAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                useShortDoctype: true
-            }
-            
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-            chunkFilename: '[id].css'
-        }),
-        new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.(sa|sc|c)ss$/,
-            cssProcessor: require('cssnano'),
-            cssProcessorPluginOptions: {
-                preset: [
-                    'default',
-                    {
-                        discardComments: { removeAll: true}
-                    }
-                ],
+  devtool: 'source-map',
+  entry: {
+    app: ['@babel/polyfill', './src/main.js'],
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /^(?!.*\.{test,min}\.js$).*\.js$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
             },
-            canPrint: true
-        })
+          },
+          {
+            loader: 'eslint-loader',
+            options: {
+              cache: true,
+              failOnError: true,
+              emitWarning: true,
+              configFile: './.eslintrc.json',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                postcssPresetEnv({
+                  browsers: 'last 2 versions',
+                  autoprefixer: {
+                    grid: true,
+                    cascade: false,
+                  },
+                  stage: 3,
+                  features: {
+                    'nesting-rules': true,
+                  },
+                }),
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-loader',
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+        exclude: path.resolve(__dirname, '/src/index.html'),
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
     ],
-    module:{
-        rules: [
-            {
-                test: /^(?!.*\.{test,min}\.js$).*\.js$/,
-                exclude: /(node_modules)/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env'],
-                        }
-                    },
-                    {
-                        loader: 'eslint-loader',
-                        options:{
-                            cache: true,
-                            failOnError: true,
-                            emitWarning: true,
-                            configFile: "./.eslintrc.json"
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options:{
-                            publicPath: '../',
-                        }
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                            importLoaders: 2,
-                        },
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                            importLoaders: 1,
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options:{
-                            ident: 'postcss',
-                            plugins: () => [
-                              postcssPresetEnv({ 
-                                    browsers: 'last 2 versions',
-                                    autoprefixer: { 
-                                        grid: true,
-                                        cascade: false
-                                    }, 
-                                    stage: 3,
-                                    features: {
-                                      'nesting-rules': true
-                                    }
-                                }
-                              ),
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.pug$/,
-                use: 'pug-loader'
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options:{
-                            name: '[name].[ext]',
-                            outputPath: 'img/',
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'fonts/'
-                        }
-                    }
-                ]
-            },
-        ]
-    }
-}
+  },
+  plugins: [
+    // new HtmlWebpackPlugin({
+    //   title: 'My App',
+    //   filename: 'index.html',
+    //   template: 'src/index.html',
+
+    //   minify: {
+    //     collapseWhitespace: false,
+    //     removeComments: true,
+    //     removeRedundantAttributes: true,
+    //     removeScriptTypeAttributes: true,
+    //     removeStyleLinkTypeAttributes: true,
+    //     useShortDoctype: true,
+    //   },
+
+    // }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.(sa|sc|c)ss$/,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: [
+          'default',
+          {
+            discardComments: { removeAll: true },
+          },
+        ],
+      },
+      canPrint: true,
+    }),
+  ],
+};
